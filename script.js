350ee9e908f77ba7b1cdd21d2e65c07d3665e1ca
Ola <olo2040@wp.pl>
2024-02-22 14:48:09 +0100

    a tag added to mobile navigation

--- script.js
+++ script.js
@@ -3,20 +3,9 @@ burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('burger-btn--active')
 })
 
-const menuSections = document.querySelectorAll('.menu-section');
-const tabsMenu = document.querySelectorAll('.menu-tab');
 
 
 
-
-const showInfo = () => {
-
-    tabsMenu.classList.toggle('is-active');
-
-}
-
-
-tabsMenu.addEventListener("click", showInfo);
 
 
 
