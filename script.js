63ef10c5dd8be3a70ef950a73c535913feacfd39
Ola <olo2040@wp.pl>
2024-03-15 23:14:27 +0100

    fixed popup closing

--- script.js
+++ script.js
@@ -11,32 +11,55 @@ const ARROW_LEFT = document.querySelector(".popup__arrow--left");
 const ARROW_RIGHT = document.querySelector(".popup__arrow--right");
 
 let currentImgIndex = 0;
+let isArrowKeyPressed = false; // Zmienna śledząca, czy klawisz strzałki w prawo został już wciśnięty
 
 const fotoScale = (e) => {
-	currentImgIndex = Array.from(THUMBNAILS).indexOf(e.target); // Ustawienie currentImgIndex na indeks klikniętej miniatury
 	POPUP.classList.remove("hidden");
 	POPUP_IMG.src = e.target.src;
+	currentImgIndex = Array.from(THUMBNAILS).indexOf(e.target); // Ustawienie currentImgIndex na indeks klikniętej miniatury
 };
 
-const fotoClose = () => {
-	POPUP.classList.add("hidden");
+const fotoClose = (event) => {
+	POPUP.classList.add("fade-out")
+	if (!POPUP.classList.contains("hidden")) {
+		setTimeout(() => {
+			POPUP.classList.add("hidden");
+			POPUP.classList.remove("fade-out");
+		}, 300);
+	} if (event) event.stopPropagation();
 };
-
-THUMBNAILS.forEach((thumbnail) => {
-	thumbnail.addEventListener("click", (e) => {
-		fotoScale(e);
-	});
-});
 
 const nextFoto = () => {
-	currentImgIndex = (currentImgIndex + 1) % THUMBNAILS.length; // Zwiększenie indeksu, z uwzględnieniem zawijania
-	POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+	if (!POPUP.classList.contains("hidden")) {
+		currentImgIndex = (currentImgIndex + 1) % THUMBNAILS.length; // Zwiększenie indeksu, z uwzględnieniem zawijania
+		POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+	}
 };
 
 const prevFoto = () => {
-	currentImgIndex = (currentImgIndex - 1 + THUMBNAILS.length) % THUMBNAILS.length;
-	POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
-}
+	if (!POPUP.classList.contains("hidden")) {
+		currentImgIndex =
+			(currentImgIndex - 1 + THUMBNAILS.length) % THUMBNAILS.length;
+		POPUP_IMG.src = THUMBNAILS[currentImgIndex].src;
+	}
+};
+
+const arrowKey = (e) => {
+	if (e.code === "ArrowRight") {
+		if (!isArrowKeyPressed) {
+			// Sprawdzenie, czy klawisz strzałki w prawo nie był wcześniej wciśnięty
+			isArrowKeyPressed = true; // Ustawienie flagi na true
+			nextFoto();
+		}
+	} else if (e.code === "ArrowLeft") {
+		if (!isArrowKeyPressed) {
+			isArrowKeyPressed = true;
+			prevFoto();
+		}
+	} else if (e.code === "Escape") {
+		fotoClose();
+	}
+};
 
 ARROW_LEFT.addEventListener("click", (event) => {
 	event.stopPropagation(); // Zatrzymuje propagację zdarzenia, aby uniknąć wywołania funkcji fotoClose
@@ -49,4 +72,19 @@ ARROW_RIGHT.addEventListener("click", (event) => {
 });
 
 
-POPUP.addEventListener("click", fotoClose);
+THUMBNAILS.forEach((thumbnail) => {
+	thumbnail.addEventListener("click", fotoScale);
+});
+
+
+document.addEventListener("keydown", arrowKey);
+// Resetowanie flagi, gdy klawisz strzałki w prawo zostanie zwolniony
+
+document.addEventListener("keyup", (e) => {
+	if (e.code === "ArrowRight" || e.code === "ArrowLeft") {
+		isArrowKeyPressed = false;
+	}
+});
+
+
+POPUP.addEventListener('click', fotoClose);
